19cc8005d9e551ba46ee052b04bcfeacdcd216a9
seoyeong98 <kimseozero98@naver.com>
2023-11-13 20:49:04 +0900

    api - 단순 문자열 데이터로 내리는 api 방식

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,6 +18,12 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("/hello-plainAPI")
+    @ResponseBody
+    public String helloPlainAPI(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
 
