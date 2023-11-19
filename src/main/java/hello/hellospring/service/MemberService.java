3f8b29dcf7837b50d9456b618fe94d04bedd6405
seoyeong98 <kimseozero98@naver.com>
2023-11-19 23:10:16 +0900

    refactor : validateDuplicateMember - 중복 회원 검증 로직

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,13 +14,16 @@ public class MemberService {
      * 회원 가입
      */
     public long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        validateDuplicateMember(member); // 중복 회원 검증
 
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
     }
 }
